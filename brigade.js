bde20358607de7abd8f6f39d45bfb37b0e50e86d
Reinhard Nägele <unguiculus@gmail.com>
2020-10-02 09:28:21 +0200

    Test with custom event
    Signed-off-by: Reinhard Nägele <unguiculus@gmail.com>

--- brigade.js
+++ brigade.js
@@ -1,12 +1,24 @@
 const { events, Job } = require("brigadier");
 
-events.on("exec", exec);
+events.on("deploy_service", deployService);
+events.on("cloudevent", deployTenant);
+//events.on("cloudevent", deployTenant);
 
-async function exec(e, p) {
-    let j1 = new Job("j1", "busybox", ["echo hello"]);
-    let j2 = new Job("j2", "busybox", ["echo goodbye"]);
+async function deployService(e, p) {
+    let j1 = new Job("j1", "busybox", ["env | sort"]);
+    let j2 = new Job("j2", "busybox", ["echo " + p]);
 
+    payload = JSON.parse(e.payload);
+    j1.env = {
+        FOO: payload.foo,
+    };
     await j1.run();
     await j2.run();
+    console.log("done");
+}
+
+async function deployTenant(e, p) {
+    let j = new Job("j", "busybox", ["echo " + e.payload]);
+    await j.run();
     console.log("done");
 }
